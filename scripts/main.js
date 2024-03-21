85323776b6576addfdb72da822f08f78a2afc28e
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-03-21 20:18:55 +0100

    creating portfolio cards and blog carousal

--- scripts/main.js
+++ scripts/main.js
@@ -34,3 +34,23 @@ logoImage.addEventListener('mouseenter', () => {
 logoImage.addEventListener('mouseleave', () => {
     logoImage.src = whiteColorSrc;
 });
+
+
+
+
+/*  gallery swiper script  */
+const gallery = new Swiper(".gallery_slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    allowTouchMove: true,
+    navigation: {
+        nextEl: ".swiper-button-prev",
+        prevEl: ".swiper-button-next",
+    },
+    pagination: {
+        el: ".gallery_slider-pagination",
+        clickable: true,
+    },
+
+});
