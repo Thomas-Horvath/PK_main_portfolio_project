ee069cac55a25f72eeb176afa17356496ca39ab8
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-03-30 19:52:22 +0100

    starting english version

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import projects from "../datas/projects.js";  // portfolio projects' datas
-import loginDatas from "../datas/emailjs_datas.js"; 
+import projectsEnglish from "../datas/projectsEnglish.js";  // portfolio projects' datas
+import loginDatas from "../datas/emailjs_datas.js";
 
 
 
@@ -27,7 +28,8 @@ navLinks.forEach((link) => {
 
 // menu links hover
 
-if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '/index.html' || currentPage === '/PK_main_portfolio_project/') {
+if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '/index.html' || currentPage === '/PK_main_portfolio_project/'
+    || currentPage === '/PK_main_portfolio_project/en/index.html' || currentPage === '/en/index.html' || currentPage === '/en/PK_main_portfolio_project/') {
     const sections = document.querySelectorAll(".section-link");
 
     window.addEventListener('scroll', () => {
@@ -39,16 +41,16 @@ if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '
             const link = document.querySelector(`.nav-links a[href="#${id}"]`);
 
             if (section.offsetTop <= top && section.offsetTop + section.offsetHeight > top) {
-            
+
                 link.classList.add('active');
             } else {
-              
+
                 link.classList.remove('active');
             }
         });
     });
 
-   
+
     const header = document.querySelector('.header');
     function changeNavBg() {
         const scrollY = window.scrollY;
@@ -56,7 +58,7 @@ if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '
 
         header.style.transition = "all 0.3s ease";
         logoImage.style.transition = "all 0.3s ease";
-        
+
         if (scrollY > 0 || windowWidth <= 992) {
             header.style.background = "#191919";
             header.style.height = "70px";
@@ -64,7 +66,7 @@ if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '
             logoImage.style.width = "45px";
             logoImage.style.height = "45px";
         }
-       
+
         else {
             header.style.height = "80px";
             header.style.background = "transparent";
@@ -86,10 +88,15 @@ if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '
 
 
 // Logo color change
-const logoImage = document.querySelector('.js-main-logo');
-const whiteColorSrc = './assets/img/Logo_Thomas_light.png';
-const mainColorSrc = './assets/img/Logo_Thomas_main_color.svg';
 
+const logoImage = document.querySelector('.js-main-logo');
+if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '/index.html' || currentPage === '/PK_main_portfolio_project/') {
+    var whiteColorSrc = './assets/img/Logo_Thomas_light.png';
+    var mainColorSrc = './assets/img/Logo_Thomas_main_color.svg';
+} else if (currentPage === '/PK_main_portfolio_project/en/index.html' || currentPage === '/en/index.html' || currentPage === '/en/PK_main_portfolio_project/') {
+    whiteColorSrc = '../assets/img/Logo_Thomas_light.png';
+    mainColorSrc = '../assets/img/Logo_Thomas_main_color.svg';
+}
 
 logoImage.addEventListener('mouseenter', () => {
     logoImage.src = mainColorSrc;
@@ -104,38 +111,39 @@ logoImage.addEventListener('mouseleave', () => {
 
 
 // blog swiper script  
-if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '/index.html' || currentPage === '/PK_main_portfolio_project/') {
-   
-const gallery = new Swiper(".gallery_slider", {
+if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '/index.html' || currentPage === '/PK_main_portfolio_project/'
+|| currentPage === '/PK_main_portfolio_project/en/index.html' || currentPage === '/en/index.html' || currentPage === '/en/PK_main_portfolio_project/') {
 
-    slidesPerView: getInitialSlidesPerView(),
-    spaceBetween: 20,
-    loop: true,
-    allowTouchMove: true,
-    navigation: {
-        nextEl: ".swiper-button-prev",
-        prevEl: ".swiper-button-next",
-    },
-    autoplay: {
-        delay: 5000,
+    const gallery = new Swiper(".gallery_slider", {
 
-    },
-    pagination: {
-        el: ".gallery_slider-pagination",
-        clickable: true,
-    },
+        slidesPerView: getInitialSlidesPerView(),
+        spaceBetween: 20,
+        loop: true,
+        allowTouchMove: true,
+        navigation: {
+            nextEl: ".swiper-button-prev",
+            prevEl: ".swiper-button-next",
+        },
+        autoplay: {
+            delay: 5000,
 
-});
+        },
+        pagination: {
+            el: ".gallery_slider-pagination",
+            clickable: true,
+        },
+
+    });
 
 
-function getInitialSlidesPerView() {
-    return window.innerWidth > 992 ? 2 : 1;
-}
+    function getInitialSlidesPerView() {
+        return window.innerWidth > 992 ? 2 : 1;
+    }
 
-window.addEventListener('resize', function () {
-    gallery.params.slidesPerView = getInitialSlidesPerView();
-    gallery.update(); 
-});
+    window.addEventListener('resize', function () {
+        gallery.params.slidesPerView = getInitialSlidesPerView();
+        gallery.update();
+    });
 
 };
 
@@ -168,10 +176,10 @@ const contactForm = document.getElementById("contact-form");
 const statusBox = document.querySelector(".form-status-box p");
 
 // emailjs login datas
-const serviceID = loginDatas[0].serviceID ;
+const serviceID = loginDatas[0].serviceID;
 const templateID = loginDatas[0].templateID;
 const templateParams = contactForm;
-const publicKey = loginDatas[0].publicKey ;
+const publicKey = loginDatas[0].publicKey;
 
 
 function sendEmail(e) {
@@ -197,8 +205,9 @@ function sendEmail(e) {
         }
     );
 }
-if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '/index.html' || currentPage === '/PK_main_portfolio_project/') {
-contactForm.addEventListener("submit", sendEmail);
+if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '/index.html' || currentPage === '/PK_main_portfolio_project/'
+    || currentPage === '/PK_main_portfolio_project/en/index.html' || currentPage === '/en/index.html' || currentPage === '/en/PK_main_portfolio_project/') {
+    contactForm.addEventListener("submit", sendEmail);
 };
 
 
@@ -210,18 +219,18 @@ contactForm.addEventListener("submit", sendEmail);
 const infoBoxes = document.querySelectorAll('.info-title');
 
 infoBoxes.forEach((box) => {
-   
-    const icon = box.querySelector('.title-icon'); 
-    const content = box.nextElementSibling; 
+
+    const icon = box.querySelector('.title-icon');
+    const content = box.nextElementSibling;
 
     box.addEventListener('click', function () {
         const isDisplayBlock = content.classList.contains('displayBlock');
 
         document.querySelectorAll('.info-content').forEach(item => item.classList.remove('displayBlock'));
-        
+
         document.querySelectorAll('.title-icon').forEach(item => item.classList.replace('fa-circle-arrow-up', 'fa-circle-arrow-down'));
 
-      
+
         content.classList.toggle('displayBlock', !isDisplayBlock);
         icon.classList.toggle('fa-circle-arrow-up', !isDisplayBlock);
         icon.classList.toggle('fa-circle-arrow-down', isDisplayBlock);
@@ -293,6 +302,59 @@ function cardRender(page, data) {
 
 `
 };
+function cardRenderEnglish(page, data) {
+    page.innerHTML += `
+    <div class="portfolio-card">
+    <img src=${data.imageSrc} alt=${data.imageAlt} class="portolio-img">
+    <h2 class="portfolio-card-title">${data.title}</h2>
+
+    <div class="portfolio-content">
+        <p class="portfolio-description">${data.cardDescription}</p>
+        <h4 class="portfolio-details">${data.technolgies}</h4>
+        <div class="btn btn-portfolio">Részletek <i class="fa-solid fa-up-right-from-square"></i>
+        </div>
+    </div>
+
+    <div class="portfolio-popup">
+        <i class="fa-regular fa-circle-xmark close-btn"></i>
+        <h2 class="portfolio-popup-header">${data.title}</h2>
+        <div class="portfolio-popup-content-container">
+
+            <img src=${data.imageSrc} alt=${data.imageAlt}>
+            <div class="content-container">
+                <div class="portfolio-popup-content-description">
+                <h3>A projekt leírása:</h3>
+                    <p>${data.popupDescription}</p>
+                    <h3>A felhasznált technikák:</h3>
+                    <ul>
+                       ${data.listItem1}
+                       ${data.listItem2}
+                       ${data.listItem3}
+                       ${data.listItem4}
+                       ${data.listItem5}
+                       ${data.listItem6}
+                    </ul>
+                    <h3>A projekt típusa:</h3>
+                    <p>${data.type}</p>
+                </div>
+                <div class="portfolio-popup-btn-group">
+                    <a href=${data.githubLink}
+                        target="_blank" class="btn popup-btn">
+                        <i class="fa-brands fa-github"></i>
+                        Code</a>
+                    <a href=${data.githubPagesLink}
+                        target="_blank" class="btn popup-btn">
+                        <i class="fa-solid fa-desktop"></i>Demo</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+`
+};
 
 
 
@@ -305,6 +367,16 @@ if (currentPage === '/PK_main_portfolio_project/index.html' || currentPage === '
 } else if (currentPage === '/PK_main_portfolio_project/projects.html' || currentPage === '/projects.html') {
     projects.forEach(data => {
         cardRender(projectPageCardWrapper, data);
+    });
+};
+
+if (currentPage === '/PK_main_portfolio_project/en/index.html' || currentPage === '/en/index.html' || currentPage === '/en/PK_main_portfolio_project/') {
+    projectsEnglish.slice(0, 3).forEach(data => {
+        cardRenderEnglish(mainPageCardWrapper, data);
+    });
+} else if (currentPage === '/PK_main_portfolio_project/en/projects.html' || currentPage === '/en/projects.html') {
+    projectsEnglish.forEach(data => {
+        cardRenderEnglish(projectPageCardWrapper, data);
     });
 };
 
